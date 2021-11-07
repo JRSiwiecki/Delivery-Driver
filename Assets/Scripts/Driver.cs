b3b56562ca7bb115df1dc0560d7f3c4e35af136c
Joseph <JRSiwiecki@gmail.com>
2021-11-07 12:35:58 -0800

    Removed rotation condition, added colliders to game objects.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -7,7 +7,6 @@ public class Driver : MonoBehaviour
     [SerializeField] float steerSpeed = 225f;
     [SerializeField] float moveSpeed = 15f;
     
-
     void Start()
     {
         
@@ -20,14 +19,9 @@ public class Driver : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         
         Debug.Log("Steer Amount: " + steerAmount);
-        Debug.Log("Move Amount: " + moveAmount);
+        Debug.Log("Move Amount: " + moveAmount);      
         
-        if (moveAmount != 0)
-        {
-            transform.Rotate(0, 0, -steerAmount);
-        }
-        
-        
+        transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
 }
