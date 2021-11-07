8bf2047b7866a090d9a4e681f84e6f30132870a4
Joseph <JRSiwiecki@gmail.com>
2021-11-07 12:51:01 -0800

    Added follow camera and stable branch.

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    // The camera's position should follow the car's position.
+    [SerializeField] GameObject followObject;
+    
+    void Update()
+    {
+        transform.position = followObject.transform.position + Vector3.back;
+    }
+}
