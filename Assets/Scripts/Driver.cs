60abb9a55fe8fdca8f13339efaba20a8225daaff
Joseph <JRSiwiecki@gmail.com>
2021-11-07 11:23:45 -0800

    Only allows rotation when car is moving.

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -18,7 +18,15 @@ public class Driver : MonoBehaviour
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         
-        transform.Rotate(0, 0, -steerAmount);
+        Debug.Log("Steer Amount: " + steerAmount);
+        Debug.Log("Move Amount: " + moveAmount);
+        
+        if (moveAmount != 0)
+        {
+            transform.Rotate(0, 0, -steerAmount);
+        }
+        
+        
         transform.Translate(0, moveAmount, 0);
     }
 }
