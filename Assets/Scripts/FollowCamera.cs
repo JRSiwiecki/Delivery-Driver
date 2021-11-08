ad27c93d172c0d436d5ae3c6501aecb776878ae5
Joseph <JRSiwiecki@gmail.com>
2021-11-08 14:49:55 -0800

    Added world borders and bridge borders.

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -6,7 +6,7 @@ public class FollowCamera : MonoBehaviour
 {
     // The camera's position should follow the car's position.
     [SerializeField] GameObject followObject;
-    
+
     void Update()
     {
         transform.position = followObject.transform.position + Vector3.back;
